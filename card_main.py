5862496b73d5b981fa3e555ecf7136a74a2359e5
martechDH <ibelieveing@gmail.com>
2018-09-14 11:42:34 +0800

    function def

---
+++ card_main.py
@@ -0,0 +1,19 @@
+import card_tool
+while True:
+    card_tool.show_menu() 
+    user_sel = input("请输入你的选项:")
+    print("你输入的选项是%s" % user_sel)
+    if user_sel in ["1", "2", "3"]:
+        
+        if user_sel == "1":
+            card_tool.now_card()
+        if user_sel == "2":
+            card_tool.display_card()
+            return
+        if user_sel == "3":
+            card_tool.search_card()
+    elif user_sel == "0":
+        pass
+        break
+    else:
+        print("输入错误")
---
+++ card_tool.py
@@ -0,0 +1,50 @@
+card_list = []
+
+def show_menu():
+
+    print("名片管理")
+    print("*" * 50)
+    print("1.添加名片")
+    print("2.显示名片")
+    print("3.查询名片")
+
+    print("0.退出")
+    print("*" * 50)
+ 
+
+def now_card():
+    print("-" * 50)
+    print("新增名片")
+    name = input("请输入你的名字:") 
+    phone = input("请输入你的电话:") 
+    qq = input("请输入你的qq:") 
+    email = input("请输入你的email:")
+
+
+    card_dict = {"name": name,
+                 "phone": phone,
+                 "qq": qq,
+                 "email": email}
+    card_list.append(card_dict)
+
+    print("名片添加成功")
+    
+def display_card():
+    print("-" * 50)
+    print("显示名片")
+    if len(card_list) == 0:
+        print("名片库无纪录")
+        #return
+    for name in ["name", "phone", "qq", "email"]:
+        print(name, end="\t\t")
+    print(" ")
+    print("=" * 50)
+    for card_dict in card_list:
+        print("%s\t\t%s\t\t%s\t\t%s\t\t" %(card_dict["name"],
+                                           card_dict["phone"],
+                                           card_dict["qq"],
+                                           card_dict["email"]))
+def search_card():
+    print("-" * 50)
+    
+    print("查询名片")
